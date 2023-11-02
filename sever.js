d6b01e3446eca06a41e47ab6f9dd7142358d0b50
NghiemHong <Hung24855@gmail.com>
2023-11-02 23:42:53 +0700

    add view engine ejs and use it

--- sever.js
+++ sever.js
@@ -1,9 +1,13 @@
 const express = require("express"); // do cài express nên có thể sử dụng nó ở đây
 const app = express(); // app express
 
+var path = require("path"); // thư viện path giúp mk có thể lấy được đường dẫn hiện tại đang đứng . Hiện tại file mk đang đứng là file sever.js
+
+app.set("view engine", "ejs"); // Khai báo sử dụng view engine ejs
+app.set("views", path.join(__dirname, "./src/views")); // Tạo đường dẫn đến nơi lưu trữ các view engine
 //Khai báo route <Điều hướng>
 app.get("/", function (req, res) {
-  res.send("Home page");
+  res.render("Home.ejs"); // render ra view engine Home.ejs
 });
 
 app.get("login", function (req, res) {
