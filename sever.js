d0c44a8c15f5819c06e437cd83102dc6bd402c9a
NghiemHong <Hung24855@gmail.com>
2023-11-04 22:15:03 +0700

    Use and config static file

--- sever.js
+++ sever.js
@@ -1,11 +1,25 @@
-const express = require("express"); // do cài express nên có thể sử dụng nó ở đây
-const app = express(); // app express
+// do cài express nên có thể sử dụng nó ở đây
+const express = require("express");
+
+// app express
+const app = express();
+
+//Config dotenv nếu muốn truy cập file .env
 require("dotenv").config();
-var path = require("path"); // thư viện path giúp mk có thể lấy được đường dẫn hiện tại đang đứng . Hiện tại file mk đang đứng là file sever.js
+
+// thư viện path giúp mk có thể lấy được đường dẫn hiện tại đang đứng . Hiện tại file mk đang đứng là file sever.js
+var path = require("path");
+
+//Sử dụng biến trong file .env
 const Port = process.env.PORT;
-const HostName = "localhost";
+
+//Config template engine
 app.set("view engine", "ejs"); // Khai báo sử dụng view engine ejs
 app.set("views", path.join(__dirname, "./src/views")); // Tạo đường dẫn đến nơi lưu trữ các view engine
+
+//Config static file
+app.use(express.static(path.join(__dirname, "./src/public")));
+
 //Khai báo route <Điều hướng>
 app.get("/", function (req, res) {
   res.render("Home.ejs"); // render ra view engine Home.ejs
